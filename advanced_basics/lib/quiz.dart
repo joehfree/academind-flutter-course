6cdafcbc0e55d9c50eb713e8e793b1aeba6d3e1e
Joe Free <joehfree@gmail.com>
2025-03-22 08:33:27 -0500

    Lecture 64:  Understanding "if" statements
    Demonstrated a different approach to setting the active screen.  Here we're doing our comparison in the build method itself, instead of a Ternary expression.  This also avoids the lifecycle issue from a couple of lectures ago since we are doing the comparison when the widget's build method is called, which is after we receive the parameters from the calling code.

--- advanced_basics/lib/quiz.dart
+++ advanced_basics/lib/quiz.dart
@@ -23,6 +23,11 @@ class _QuizState extends State<Quiz> {
 
   @override
   Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -36,10 +41,7 @@ class _QuizState extends State<Quiz> {
               ],
             ),
           ),
-          child:
-              activeScreen == 'start-screen'
-                  ? StartScreen(switchScreen)
-                  : const QuestionsScreen(),
+          child: screenWidget,
         ),
       ),
     );
