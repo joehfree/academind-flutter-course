13bc4c5c7e72213688cfb1c4fa066d50825125d7
Joe Free <joehfree@gmail.com>
2025-03-24 12:30:39 -0500

    Lectures 77 - 78:  Passing Data via Functions Across Widgets
    We are now keeping track of the selected answers in the _QuizState and checking to see if we've answered all the questions.  For now, we return to the start screen.

--- advanced_basics/lib/questions_screen.dart
+++ advanced_basics/lib/questions_screen.dart
@@ -4,7 +4,9 @@ import 'package:advanced_basics/answer_button.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -15,7 +17,8 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion() {
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
     setState(() {
       currentQuestionIndex++;
     });
@@ -45,7 +48,12 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             const SizedBox(height: 30),
             //map the answer buttons to new widgets
             ...currentQuestion.getShuffledAnswers().map((answer) {
-              return AnswerButton(answerText: answer, onTap: answerQuestion);
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {
+                  answerQuestion(answer);
+                },
+              );
             }),
           ],
         ),
--- advanced_basics/lib/quiz.dart
+++ advanced_basics/lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:advanced_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:advanced_basics/start_screen.dart';
+import 'package:advanced_basics/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -13,19 +14,32 @@ class Quiz extends StatefulWidget {
 
 class _QuizState extends State<Quiz> {
   var activeScreen = 'start-screen';
+  List<String> selectedAnswers = [];
 
   void switchScreen() {
     //setstate causes Flutter to re-execute the build method on the widget so the UI will update
     setState(() {
+      selectedAnswers = [];
       activeScreen = 'questions-screen';
     });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      //then we ran out of questions
+      setState(() {
+        activeScreen = 'start-screen';
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     Widget screenWidget = StartScreen(switchScreen);
     if (activeScreen == 'questions-screen') {
-      screenWidget = const QuestionsScreen();
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
     }
 
     return MaterialApp(
