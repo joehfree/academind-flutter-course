b69e47138724fd23fa52c1371cd5deaaee134fef
Joe Free <joehfree@gmail.com>
2025-03-27 08:33:34 -0500

    Assignment:  Modify QuestionSummary to use the new SummaryItem widget.

--- advanced_basics/lib/questions_summary/questions_summary.dart
+++ advanced_basics/lib/questions_summary/questions_summary.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:advanced_basics/questions_summary/summary_item.dart';
 
 //this will be one list item on the restults screen
 class QuestionsSummary extends StatelessWidget {
@@ -10,45 +10,14 @@ class QuestionsSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 400,
       child: SingleChildScrollView(
         child: Column(
           children:
               summaryData.map((data) {
-                return Row(
-                  children: [
-                    Text(
-                      ((data['question_index'] as int) + 1).toString(),
-                      style: GoogleFonts.lato(
-                        fontSize: 24,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Text(
-                            data['question'] as String,
-                            style: GoogleFonts.lato(
-                              fontSize: 18,
-                              color: Colors.white,
-                            ),
-                          ),
-                          const SizedBox(height: 5),
-                          Text(
-                            data['user_answer'] as String,
-                            style: GoogleFonts.lato(
-                              fontSize: 18,
-                              color: Colors.white,
-                            ),
-                          ),
-                          Text(data['correct_answer'] as String),
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              }).toList(),
+                return SummaryItem(data);
+              },
+              ).toList(),
         ),
       ),
     );
