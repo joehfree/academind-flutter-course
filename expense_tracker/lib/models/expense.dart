85626c4a18caac4970332acac73c82d68fa7bf8d
Joe Free <joehfree@gmail.com>
2025-03-28 07:15:16 -0500

    Lecture 105:  Using icons and formatting dates
    Added a formatting package to format dates and added a map of icons for expense categories, which we now use in our ExpenseItem class.

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
 
+//create utility object to format dates
+final formatter = DateFormat.yMd();
+
+//create utility object to provide unique IDs
 const uuid = Uuid();
 
 enum Category {
@@ -9,6 +15,14 @@ enum Category {
   work
 }
 
+//create a map of icons to categories
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
 class Expense {
   Expense({required this.title, required this.amount, required this.date, required this.category})
     : id = uuid.v4();
@@ -17,5 +31,10 @@ class Expense {
   final String title;
   final double amount;
   final DateTime date;
-  final Category category;  
+  final Category category;
+
+  String get formattedDate {
+    //we can use our formatter object to format the date
+    return formatter.format(date);
+  }
 }
