3ed3ab5929006108435db4d0a3dc3b99bcf580cb
Joe Free <joehfree@gmail.com>
2025-03-28 08:26:28 -0500

    Lecture 113:  Closing the modal manually
    Here we added Navigator.pop() to close the modal overlay

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -45,7 +45,9 @@ class _NewExpenseState extends State<NewExpense> {
           Row(
             children: [
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context);
+                },
                 child: const Text('Cancel'),
               ),
               ElevatedButton(
