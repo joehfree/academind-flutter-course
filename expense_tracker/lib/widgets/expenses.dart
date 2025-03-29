0dd42ffaf5bf2b53cc9e774c0d50554fafa1ea48
Joe Free <joehfree@gmail.com>
2025-03-29 06:50:57 -0500

    Lecture 122:  Showing and managing SnackBars
    Here we used ScaffoldMessenger to create a SnackBar on the screen when the user deletes an expense entry.  The SnackBar allows the user to undo the delete action.  We also keep track of the expense's index before deletion, so we have a chance to reinsert it into the list after it has been deleted, if the user chooses to undo that action.  We're clearing any previous SnackBar in case the user deletes multiple items quickly.

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -43,13 +43,40 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('Use the plus button to create your first expense entry.'),
+    );
+    
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
@@ -61,12 +88,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The Chart'),
           //we need to wrap our list with Expanded because it is essentially a column within a column
-          Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: _removeExpense,
-            ),
-          ),
+          Expanded(child: mainContent),
         ],
       ),
     );
