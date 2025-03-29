cd7a10dd14827a0643ccea804cae36b5424c71a5
Joe Free <joehfree@gmail.com>
2025-03-29 07:10:49 -0500

    Lecture 125:  Setting and using a ColorScheme
    Here we are demonstrating a recommended way to theme an app.  Instead of creating an entirely new ThemeData for MaterialApp, and then configuring every property, we can simply create a ColorScheme from a seed color, and then use those colors to override things in the theme.  In this example, we've only overridden a couple things in our AppBarTheme.

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      //we use copyWith so we can override some stuff without having to completely configure the whole ThemeData
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        //again here we're only overriding a couple things instead of creating a whole new AppBarTheme
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: Expenses(),
     ),
   );
